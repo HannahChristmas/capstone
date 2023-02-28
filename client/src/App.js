e0e77aaf70b242d1d537cb5d3e8830dfa2149eb7
HannahChristmas <hc109909@gmail.com>
2023-02-28 13:30:49 -0500

    MUI installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <>
     <h1>Hello</h1>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
     </>
   );
 }
