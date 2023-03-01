61514e32d27700d7efeab70bc533c6b05a768305
HannahChristmas <hc109909@gmail.com>
2023-03-01 15:10:24 -0500

    Login Page Finalized

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
-    </>
+    <header className="header-container">
+        <h1 id="login-logo-cincy-social">CINCY SOCIAL</h1>
+      <div>
+      <h3 id="login-nav">LOGIN</h3>
+      <h3 id="login-nav">CREATE ACCOUNT</h3>
+      </div>
+   </header>
+   <div className="login-welcome-div">
+    <h1 id="login-header-whats-happening">what's happening in cincy?</h1>
+    <p className="welcome-sentences">find something fun to do.<br></br>
+    plan it with your friends.<br></br>
+    document it here.</p>
+   </div>
+   <img src="https://i.ibb.co/qFPpqCQ/skyline3.png" alt="skyline"/>
+   </>
   );
 }
 
