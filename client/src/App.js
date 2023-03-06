b85db15e633615c79ecaa0fa1ac1c8b83a2b9287
HannahChristmas <hc109909@gmail.com>
2023-03-06 13:37:10 -0500

    Login Screen Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,31 @@
-import logo from './logo.svg';
 import './index.css';
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import React, { useEffect, useState } from "react";
+import LoginScreen from './components/LoginScreen.js'
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  // const [activities, setActivities] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/activities")
+  //   .then(r => r.json())
+  //   .then(activities => setActivities(activities))
+  // }, [])
+
+  useEffect(() => {
+    fetch("/me").then((res) => {
+      if (res.ok) {
+        res.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <LoginScreen onLogin={setUser}/>
+
   return (
     <>
-    <header className="header-container">
-        <h1 id="login-logo-cincy-social">CINCY SOCIAL</h1>
-      <div>
-      <h3 id="login-nav">LOGIN</h3>
-      <h3 id="login-nav">CREATE ACCOUNT</h3>
-      </div>
-   </header>
-   <div className="login-welcome-div">
-    <h1 id="login-header-whats-happening">what's happening in cincy?</h1>
-    <p className="welcome-sentences">find something fun to do.<br></br>
-    plan it with your friends.<br></br>
-    document it here.</p>
-   </div>
-   <img src="https://i.ibb.co/qFPpqCQ/skyline3.png" alt="skyline"/>
+      <h1>Hey what's up. I'll put a nav bar here.</h1>
    </>
   );
 }
