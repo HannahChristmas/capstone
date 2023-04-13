861248c87df5bba336f472b0a0c69d7541b986ea
HannahChristmas <hc109909@gmail.com>
2023-04-13 10:53:11 -0400

    Click outside of menu to close

--- client/src/components/LoginNav.js
+++ client/src/components/LoginNav.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect, useRef } from 'react'
 import {Link} from 'react-router-dom'
 import profile from '../profile.png'
 import { UserContext } from "../UserContext";
@@ -10,9 +10,20 @@ function LoginNav() {
     const navigate = useNavigate();
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    let menuRef = useRef();
+
+    useEffect(() => {
+        document.addEventListener('mousedown', (event) => {
+            if (!menuRef.current.contains(event.target)) {
+                setIsMenuOpen(false);
+            }
+        });
+    });
 
     function handleLogoutClick() {
         fetch("/logout", { method: "DELETE" }).then((r) => {
@@ -29,6 +40,7 @@ function LoginNav() {
         <>    
         <nav className="nav">
         <Link to='/' className="site-title">CINCY SOCIAL</Link>
+        <div ref={menuRef} id="hey">
         {isMenuOpen ? (
             <ul>
                 <li>
@@ -53,35 +65,22 @@ function LoginNav() {
         ) : (
             <img src={profile} onClick={toggleMenu} alt="default-profile"></img>
         )}
+        </div>
       </nav>
 <nav className="activity-nav">
         <h1>Welcome, !</h1>
         {/* <h1>Welcome, {user.username}!</h1> */}
-
 <ul>
     <li>
         <Link to='/activities' className="login-nav">ALL ACTIVITIES</Link>
-
-        {/* <a href="/activities" className="login-nav">ALL ACTIVITIES</a> */}
     </li>
     <li>
         <Link to='/interested' className="login-nav">I'M INTERESTED</Link>
-
-        {/* <a href="/interested" className="login-nav">I'M INTERESTED</a> */}
     </li>
     <li>
         <Link to='/visited' className="login-nav">I'VE BEEN THERE</Link>
-
-        {/* <a href="/visited" className="login-nav">I'VE BEEN THERE</a> */}
     </li>
 </ul>
-
-
-{/* <h1 id="login-logo-cincy-social">CINCY SOCIAL</h1> */}
-  {/* <div>
-    <h3 id="login-nav" onClick={loginClick}>LOGIN</h3>
-    <h3 id="login-nav">CREATE ACCOUNT</h3>
-  </div> */}
 </nav>
 </>
     )
