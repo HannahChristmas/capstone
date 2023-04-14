a751cda3a38091682cbc8ef7bfa329c10d42894e
HannahChristmas <hc109909@gmail.com>
2023-04-14 10:44:41 -0400

    Activity model update

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,9 @@
 class Activity < ApplicationRecord
+    has_many :user_activities
+    
+    has_many :interested_users, through: :user_activities, source: :user, conditions: { user_activities: {interested: true }}
+    has_many :visited_users, through: :user_activities, source: :user, conditions: { user_activities: { visited: true } }
+    has_many :review_content
+
+
 end
