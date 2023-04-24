3501aa23cde99f51f5ac73f05cd3af03d415df6d
HannahChristmas <hc109909@gmail.com>
2023-04-24 13:06:36 -0400

    Add show to activities_controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,4 +5,9 @@ class ActivitiesController < ApplicationController
         activities = Activity.all 
         render json: activities 
     end
+
+    def show 
+        activity = Activity.find_by(id: params[:id])
+        render json: activity
+    end
 end
