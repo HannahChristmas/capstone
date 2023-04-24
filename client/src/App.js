4635ccf06b738c8bc81f145a8e21d3f45cd98a24
HannahChristmas <hc109909@gmail.com>
2023-04-24 14:00:09 -0400

    Interested works from App.js for AllActivities.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,92 +12,104 @@ import UserProfile from './pages/UserProfile';
 import { UserContext } from './UserContext';
 
 function App() {
+ 
+    const [user, setUser] = useState(null);
+    const [activities, setActivities] = useState([]);
+    const [selectedActivity, setSelectedActivity] = useState(null);
+    const [interestedVariable, setInterestedVariable] = useState(null);
+    const [interestedButtonText, setInterestedButtonText] = useState(null);
 
-  const [user, setUser] = useState(null);
-  const [activities, setActivities] = useState([]);
-  const [selectedActivity, setSelectedActivity] = useState(null);
-  const [interestedVariable, setInterestedVariable] = useState(null);
-  const [interestedButtonText, setInterestedButtonText] = useState(null);
 
-  useEffect(() => {
+useEffect(() => {
     fetch("/me").then((res) => {
-      if (res.ok) {
-        res.json().then((user) => {
-          if (user !== null) {
+        if (res.ok) {
+            res.json().then((user) => {
+        if (user !== null) {
             setUser(user)
-          }
+        }
         });
-      }
+    }
     });
-  }, []);
+}, []);
 
-  useEffect(() => {
+useEffect(() => {
     fetch("/activities")
     .then(r => r.json())
     .then(activities => setActivities(activities))
-  }, [])
+}, [])
 
-  useEffect(() => {
-    if (selectedActivity) {
-      const interested = selectedActivity.user_activities[0]?.interested;
-      setInterestedVariable(interested);
-      setInterestedButtonText(interested ? 'Remove from Interests' : 'Add to Interests');
+    useEffect(() => {
+        if (selectedActivity) {
+        const interested = selectedActivity.user_activities[0]?.interested;
+        setInterestedVariable(interested);
+        setInterestedButtonText(interested ? 'Remove from Interests' : 'Add to Interests');
     } else {
-      setInterestedVariable(null);
-      setInterestedButtonText(null);
+        setInterestedVariable(null);
+        setInterestedButtonText(null);
     }
-  }, [selectedActivity]);
+}, [selectedActivity]);
 
 
-  function interestedClick() {
-    if(selectedActivity) {
-      const updatedInterest = { ...selectedActivity, interested: !selectedActivity.interested }
+function interestedClick() {
+    if (selectedActivity) {
+        let isUserInterested = false 
 
-    fetch('/user_activities', {
-      method: 'POST',
-      body: JSON.stringify({
+        selectedActivity?.user_activities.forEach(item => {
+            if (item.user_id == user.id) { 
+                item.interested = !item.interested
+                isUserInterested = item.interested
+                console.log("isUserInterested from intClick: ", isUserInterested)
+            } else {
+                console.log("No. selectedActivity.user_activities doesn't exist")
+            }
+        })
+
+    fetch('/user_activities', { // nothing changes here really
+        method: 'POST',
+        body: JSON.stringify({
         user_id: user.id,
-        activity_id: selectedActivity.id, 
-        interested: updatedInterest.interested,
-      }),
-      headers: {
+        activity_id: selectedActivity.id,
+        interested: isUserInterested, // accept this
+        }),
+        headers: {
         'Content-Type': 'application/json'
-      }
+        }
     }).then(response => {
-      if (response.ok) {
-        setSelectedActivity(updatedInterest)
-        setActivities((prevActivities) => {
-          const newActivities = prevActivities.map((activity) => 
-            activity.id === updatedInterest.id ? updatedInterest : activity
-          );
-          return newActivities;
+        if (response.ok) {
+    setSelectedActivity(selectedActivity)
+    setActivities((prevActivities) => {
+        const newActivities = prevActivities.map((activity) =>
+        activity.id === selectedActivity.id ? selectedActivity : activity
+        );
+        return newActivities;
         });
-        setInterestedButtonText(updatedInterest.interested ? 'Remove from Interests' : 'Add to Interests');
-      } else {
+    setInterestedButtonText(isUserInterested ? 'Remove from Interests' : 'Add to Interests');
+        } else {
         console.log("interested no work not nice")
-      }
+    }
     });
-  }}
+    }
+    }
 
-  return (
-    <>
-      <UserContext.Provider value={{ user, setUser }}>
-        <LoginNav />
-        <main>
-          <Routes>
-            <Route path="/" element={<LoginHomeScreen/>}></Route>
-            <Route path="/login" element={<LoginForm/>}></Route>
-            <Route path="/logout" element={<LoginHomeScreen/>}></Route>
-            <Route path="/create-account" element={<CreateAccount/>}></Route>
-            <Route path="/user-profile" element={<UserProfile/>}></Route>
-            <Route path="/activities" element={<AllActivities activities={activities} selectedActivity={selectedActivity} setSelectedActivity={setSelectedActivity} interestedClick={interestedClick} interestedButtonText={interestedButtonText}/>}></Route>
-            <Route path="/interested" element={<Interested setSelectedActivity={setSelectedActivity} interestedClick={interestedClick} activities={activities}/>} ></Route>
-            <Route path="/visited" element={<Visited/>}></Route>
-          </Routes>
-        </main>
-    </UserContext.Provider>
-   </>
-  );
+    return (
+        <>
+        <UserContext.Provider value={{ user, setUser }}>
+            <LoginNav />
+            <main>
+                <Routes>
+                    <Route path="/" element={<LoginHomeScreen />}></Route>
+                    <Route path="/login" element={<LoginForm />}></Route>
+                    <Route path="/logout" element={<LoginHomeScreen />}></Route>
+                    <Route path="/create-account" element={<CreateAccount />}></Route>
+                    <Route path="/user-profile" element={<UserProfile />}></Route>
+                    <Route path="/activities" element={<AllActivities activities={activities} selectedActivity={selectedActivity} setSelectedActivity={setSelectedActivity} interestedClick={interestedClick} interestedButtonText={interestedButtonText} />}></Route>
+                    <Route path="/interested" element={<Interested setSelectedActivity={setSelectedActivity} interestedClick={interestedClick} activities={activities} />} ></Route>
+                    <Route path="/visited" element={<Visited />}></Route>
+                </Routes>
+            </main>
+        </UserContext.Provider>
+        </>
+    );
 }
 
 
