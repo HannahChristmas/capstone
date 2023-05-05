7a8b1def94c3b2156c83b568f18c4269d7f39397
HannahChristmas <hc109909@gmail.com>
2023-05-04 20:42:08 -0400

    Public profile update shows if no interests

--- client/src/pages/PublicProfile.js
+++ client/src/pages/PublicProfile.js
@@ -5,6 +5,9 @@ function PublicProfile(){
     const { id } = useParams();
     const [publicProfile, setPublicProfile] = useState(null);
 
+    const publicVisited = publicProfile?.user_activities.filter(activity => activity.visited)
+    const publicInterested = publicProfile?.user_activities.filter(activity => activity.interested)
+
     useEffect(() => {
         fetch(`/users/${id}`)
         .then(r => r.json())
@@ -21,23 +24,38 @@ function PublicProfile(){
             <h3>{publicProfile?.username}</h3>
             <p>bio</p>
             <h4>Activities user is interested in:</h4>
-                {publicProfile?.user_activities.filter(activity => activity.interested)
-                .map(activity => {
-                    const interestedList = publicProfile.activities.find(a => a.id === activity.activity_id)
-                    return (
-                        <p key={interestedList.id}>{interestedList.title}</p>
-                    )
-                })}
+                {publicInterested?.length === 0 ? (
+                    <p>no</p>
+                ) : (
+                    publicInterested?.map(activity => {
+                        const interestedList = publicProfile.activities.find(a => a.id === activity.activity_id);
+                        return (
+                            <p key={interestedList.id}>{interestedList.title}</p>
+                        );
+                    })
+                )}
 
             <h4>Activities user has visited:</h4>
-                {publicProfile?.user_activities.filter(activity => activity.visited)
-                    .map(activity => {
-                        const visitedList = publicProfile.activities.find(a => a.id === activity.activity_id)   
+                {publicVisited?.length === 0 ? (
+                    <p>no</p>
+                ) : (
+                publicVisited?.map(activity => {
+                        const visitedList = publicProfile.activities.find(a => a.id === activity.activity_id);
                         return (
                             <p key={visitedList.id}>{visitedList.title}</p>
-                        )
-                })}        
-        </>
+                        );
+                    })
+                )}
+                
+            <div>
+                <h1>User Interests:</h1>
+                {publicProfile?.user_interests.map(userInterest => {
+                    const interest = publicProfile?.interests.find(interest => interest.id === userInterest.interest_id);
+                    if (!interest) return null; // ignore invalid user interests
+                    return <p key={interest.id}>{interest.name}</p>;
+                })}
+            </div>   
+    </>
         
     )
 }
