047b1f1319ed4358a6e5f8947b08139947f5a5e5
HannahChristmas <hc109909@gmail.com>
2023-05-04 21:03:25 -0400

    PublicProfile update

--- client/src/pages/PublicProfile.js
+++ client/src/pages/PublicProfile.js
@@ -8,6 +8,8 @@ function PublicProfile(){
     const publicVisited = publicProfile?.user_activities.filter(activity => activity.visited)
     const publicInterested = publicProfile?.user_activities.filter(activity => activity.interested)
 
+    const name = publicProfile?.username
+
     useEffect(() => {
         fetch(`/users/${id}`)
         .then(r => r.json())
@@ -19,13 +21,12 @@ function PublicProfile(){
 
     return (
         <>
-            <h1>Great</h1>
+            <h1>{name}'s profile</h1>
             <p>user's picture</p>
-            <h3>{publicProfile?.username}</h3>
-            <p>bio</p>
-            <h4>Activities user is interested in:</h4>
+            <p>{publicProfile?.bio}</p>
+            <h4>activities {name} wants to check out:</h4>
                 {publicInterested?.length === 0 ? (
-                    <p>no</p>
+                    <p>no activities on the wishlist right now</p>
                 ) : (
                     publicInterested?.map(activity => {
                         const interestedList = publicProfile.activities.find(a => a.id === activity.activity_id);
@@ -35,9 +36,9 @@ function PublicProfile(){
                     })
                 )}
 
-            <h4>Activities user has visited:</h4>
+            <h4>activities {publicProfile?.username} has visited:</h4>
                 {publicVisited?.length === 0 ? (
-                    <p>no</p>
+                    <p>{name} hasn't been anywhere in Cincinnati... yet!</p>
                 ) : (
                 publicVisited?.map(activity => {
                         const visitedList = publicProfile.activities.find(a => a.id === activity.activity_id);
@@ -48,10 +49,10 @@ function PublicProfile(){
                 )}
                 
             <div>
-                <h1>User Interests:</h1>
+                <h1>{publicProfile?.username} likes:</h1>
                 {publicProfile?.user_interests.map(userInterest => {
                     const interest = publicProfile?.interests.find(interest => interest.id === userInterest.interest_id);
-                    if (!interest) return <p>nope</p> // ignore invalid user interests
+                    if (!interest) return <p>no interests have been chosen yet</p> // ignore invalid user interests
                     return <p key={interest.id}>{interest.name}</p>;
                 })}
             </div>   
