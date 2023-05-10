f840d53d13266cc7fcb1e59c215f91416281f1a0
HannahChristmas <hc109909@gmail.com>
2023-05-10 10:40:15 -0400

    EditProfile styled

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -1,5 +1,23 @@
 import { useContext, useState, useEffect, useRef } from 'react'
 import { UserContext } from "../UserContext";
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+// import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Input from '@mui/material/Input';
+import { Stack } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  height: '40rem',
+  display: 'flex',
+  justifyContent: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function EditProfile () {
     const { user, setUser, selectedImage, setSelectedImage } = useContext(UserContext)
@@ -47,7 +65,6 @@ function EditProfile () {
         }
         })
 
-
     useEffect(() => {
         if (user) {
             setUsername(user.username)
@@ -72,7 +89,6 @@ function EditProfile () {
       })
       }, [setUserInterests])
 
-
     const handleInterestClick = (interestId) => {
       fetch("/user_interests", {
         method: 'POST',
@@ -90,8 +106,6 @@ function EditProfile () {
         })        
     }
 
-    // console.log("UI after post request: ", userInterests)
-
     const handleInterestDelete = (interestId) => {
       fetch(`/user_interests/${interestId}`, {
         method: 'DELETE',
@@ -106,15 +120,15 @@ function EditProfile () {
       .catch(error => console.error(error));    
     }
 
-
     function handlePostUpdateSubmit(e) {
         e.preventDefault()
-
         const formData = new FormData();
-        formData.append('image', selectedImage)
         formData.append('username', username)
         formData.append('bio', bio)
-        console.log(formData)
+
+        if (selectedImage) {
+          formData.append('image', selectedImage);
+        }
 
         fetch(`/me`, {
           method: "PATCH",
@@ -131,54 +145,77 @@ function EditProfile () {
         })
       }
 
-
-
     if (!user) {
         return <p>Profile loading...</p>
     }
     
     return ( 
         <>
-        <div id="main-profile-container">
-          <div id="edit-profile-container">
-            <h1>{user.username}</h1>
-            <p>{user.bio}</p>
-              <form onSubmit={handlePostUpdateSubmit} id="login-form">
-              <label>
-                    image:
-                    <input type="file"
-                        onChange={e => setSelectedImage(e.target.files[0])}
-                        ref={imageUpload}
-                        accept="image/png, image/jpeg"
-                    />
-                </label>
-                  <label>
-                      username:
-                      <input
-                          type="text"
-                          id="username"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                      />
-                  </label><br/>
-                  <label>
-                      bio: 
-                      <input
-                          type="text"
-                          id="bio"
-                          value={bio ? bio : "Enter bio here"}
-                          onChange={(e) => setBio(e.target.value)}
-                      />
-                  </label><br/>
-                  <button type="submit">{isLoading ? "Loading..." : "save changes"}</button><br/>
-                  <label>
-                      {errors.map((err) => (
-                          <p key={err}>{err}</p>
-                      ))}
-                  </label>
-              </form>
-            </div>
+        <div id="CENTER-THIS-BITCH">
+          {/* MAIN GRID */}
+        <Grid  width="85%" container spacing={1} justifyContent="center">
 
+          {/* GRID WITH PIC */}
+          <Grid container display="flex" justifyContent="center">
+            <Item>
+              <Stack>
+                <h1>{user.username}</h1>
+                <p>{user.bio}</p><br></br>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                  <img src={user.image} id="profile-picture" alt="pro-pic"/>
+                </div><br></br><br></br>
+                  <form onSubmit={handlePostUpdateSubmit}>
+                  <TextField 
+                        id="outlined-basic" 
+                        label="username" 
+                        variant="outlined"
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)}
+                        sx={{ width: '90%' }}
+                      /><br></br><br></br>
+                      <TextField 
+                        id="outlined-basic" 
+                        label="bio" 
+                        variant="outlined"
+                        value={bio ? bio : "Enter bio here"} 
+                        onChange={(e) => setBio(e.target.value)}
+                        sx={{ width: '90%' }}
+                      /><br></br><br></br>
+                  <Input
+                    type="file"
+                    onChange={e => setSelectedImage(e.target.files[0])}
+                    ref={imageUpload}
+                    accept="image/png, image/jpeg"
+                    sx={{ width: '90%' }}
+                  /><br></br><br></br>             
+                  <Button type="submit">{isLoading ? "Loading..." : "update profile"}</Button>
+                  <label>
+                    {errors?.map((err) => (
+                        <p key={err}>{err}</p>
+                    ))}
+                  </label>
+                </form>
+              </Stack>
+            </Item>
+          </Grid>
+          
+          {/* GRID WITH INTERESTS */}
+          <Grid xs={8}>
+            <Item>
+              <Stack>
+                <TextField 
+                  label="Search" 
+                  variant="outlined" 
+                  sx={{ width: '100%' }}
+                />
+              </Stack>
+            </Item>
+          </Grid>
+        </Grid>
+        </div>
+
+        <br></br>
+        <div id="main-profile-container">
             <div className="activity-search-container">
                 <h4>Search Bar</h4>
                 <div>
@@ -198,7 +235,6 @@ function EditProfile () {
                 </select><br></br>
                 <button onClick={handleReset}>Reset</button>
                 </div>
-                
             </div>
 
             <div id="add-interests-container">
